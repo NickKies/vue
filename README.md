5cb7f388ad9988fb22fd59df8fb1c8eeb056e4b4
YoonSung Kim <opqudk@gmail.com>
2020-06-18 18:14:05 +0900

    vue start!

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Vue.js 공식문서로 시작하는 Vue.js
+
+Vue.js DevTool (크롬)
+
+step 1 :[다운로드 링크](https://chrome.google.com/webstore/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd)
+step 2 : 크롬 설정 - 파일 URL에 대한 액세스 허용
---
+++ veuMastary/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: 'Socks.jpg',
+        inventory: 8,
+        //inStock: false,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"]
+    }
+})
