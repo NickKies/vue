f866711b0a160ed1e0ceb0b16e475b5dff19c8e5
YoonSung Kim <opqudk@gmail.com>
2020-06-22 18:49:33 +0900

    Event Handling

--- veuMastary/main.js
+++ veuMastary/main.js
@@ -5,6 +5,27 @@ var app = new Vue({
         image: 'Socks.jpg',
         inventory: 8,
         //inStock: false,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"]
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "Socks.jpg"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "BlueSocks.jpg"
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1
+        },
+        updateProduct(variantIamge) {
+            this.image = variantIamge
+        }
     }
 })
