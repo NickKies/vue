2e31e8760f7ed6cc6b486f31350f3176821b578d
YoonSung Kim <opqudk@gmail.com>
2020-06-25 18:27:03 +0900

    tabs

--- veuMastary/main.js
+++ veuMastary/main.js
@@ -1,3 +1,5 @@
+var eventBus = new Vue()
+
 Vue.component('product', {
     props: {
       premium: {
@@ -42,19 +44,9 @@ Vue.component('product', {
 
             </div>
             
-            <div>
-                <h2>Reviews</h2>
-                <p v-if="!reviews.length">There are no reviews yet.</p>
-                <ul>
-                    <li v-for="review in reviews">
-                        <p>{{ review.name }}</p>
-                        <p>Rating: {{ review.rating }}</p>
-                        <p>{{ review.review }}</p>
-                    </li>
-                </ul>
-            </div>
+            <product-tabs :reviews="reviews"></product-tabs>
             
-            <product-review @review-submitted="addReview"></product-review>
+
         </div>
     `,
     data() {
@@ -92,9 +84,6 @@ Vue.component('product', {
         updateProduct(index) {
             this.selectedVariant = index
         },
-        addReview(productReview) {
-            this.reviews.push(productReview)
-        }
     },
     computed: {
         title() {
@@ -112,6 +101,11 @@ Vue.component('product', {
             }
             return 2.99
         }
+    },
+    mounted() {
+        eventBus.$on('review-submitted', productReview => {
+            this.reviews.push(productReview)
+        })
     }
 })
 
@@ -164,20 +158,70 @@ Vue.component('product-review', {
                     review: this.review,
                     rating: this.rating
                 }
-                this.$emit('review-submitted', productReview)
+                eventBus.$emit('review-submitted', productReview)
                 this.name = null,
                     this.review = null,
                     this.rating = null
             } else {
-                if(!this.name) alert("Please correct the following error:</br> Name required.") //this.errors.push("Name required.")
-                if(!this.review) alert("Please correct the following error:</br> review required.") //this.errors.push("Review required.")
-                if(!this.rating) alert("Please correct the following error:</br> rating required.") //this.errors.push("Rating required.")
-
+                if(!this.name) {
+                    alert("Please correct the following error: Name required.")
+                    return false;
+                } //this.errors.push("Name required.")
+                if(!this.review) {
+                    alert("Please correct the following error: review required.")
+                    return false
+                } //this.errors.push("Review required.")
+                if(!this.rating) {
+                    alert("Please correct the following error: rating required.") //this.errors.push("Rating required.")
+                    return false
+                }
             }
         }
     }
 })
 
+Vue.component('product-tabs', {
+    props: {
+        reviews: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+        <div>
+            <span class="tab"
+                    :class="{ activeTab: selectedTab === tab }" 
+                    v-for="(tab, index) in tabs" 
+                    :key="index"
+                    @click="selectedTab = tab"
+                    >
+                    {{ tab }}
+            </span>
+        
+            <div v-show="selectedTab === 'Reviews'">
+                <h2>Reviews</h2>
+                <p v-if="!reviews.length">There are no reviews yet.</p>
+                <ul>
+                    <li v-for="review in reviews">
+                        <p>{{ review.name }}</p>
+                        <p>Rating: {{ review.rating }}</p>
+                        <p>{{ review.review }}</p>
+                    </li>
+                </ul>
+            </div>
+            
+            <product-review 
+            v-show="selectedTab === 'Make a Review'"
+            ></product-review>
+        </div>
+    `,
+    data() {
+        return {
+            tabs: ['Reviews', 'Make a Review'],
+            selectedTab: 'Reviews'
+        }
+    }
+})
 
 var app = new Vue({
     el: '#app',
